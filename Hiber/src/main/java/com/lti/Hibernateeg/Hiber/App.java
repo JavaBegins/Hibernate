c6151ef47d9c051d1dabea4f32e987884f63c8f1
pcuser <beginsjava144@gmail.com>
2019-11-06 03:05:48 -0800

    tiru

--- Hiber/src/main/java/com/lti/Hibernateeg/Hiber/App.java
+++ Hiber/src/main/java/com/lti/Hibernateeg/Hiber/App.java
@@ -1,13 +1,25 @@
 package com.lti.Hibernateeg.Hiber;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Configuration configuration= new Configuration().configure();
+    	StandardServiceRegistryBuilder builder=new StandardServiceRegistryBuilder()
+    			.applySettings(configuration.getProperties());
+    	SessionFactory factory=configuration.buildSessionFactory(builder.build());
+    	Session session= factory.openSession();
+    	Emp employee=new Emp();
+    	employee.setEmpName("Kavi");
+    	employee.setBranch("Chennai");
+    	session.beginTransaction();
+    	session.save(employee);
+    	session.getTransaction().commit();
+
     }
 }
